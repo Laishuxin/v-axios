3dd71a2f47974a3c006ffd57f447b3247bfbc166
Laishuxin <15622971239@163.com>
2022-10-23 17:43:48 +0800

    test: 3. 添加对拦截器的测试

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,11 +9,29 @@
       使用原始的 axios 发起请求
     </el-button>
   </div>
+
+  <div>
+    <h1>
+      测试基础的拦截器
+    </h1>
+    <el-button @click="handleTestRequestInterceptor()">
+      测试请求拦截器
+    </el-button>
+
+    <el-button @click="handleTestResponseInterceptor()">
+      测试响应拦截器
+    </el-button>
+
+    <el-button @click="handleTestResponseCatchInterceptor()">
+      测试响应异常拦截器
+    </el-button>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
+import { VAxios } from '../../lib/src/v-axios'
 
 export default {
   setup() {
@@ -39,8 +57,72 @@ export default {
         })
     }
 
+    const handleTestRequestInterceptor = () => {
+      const vAxios = new VAxios({
+        transform: {
+          requestInterceptors(config, options) {
+            const headers = config.headers
+            headers['Authorization'] = `Bearer fake token`
+            config.headers = headers
+
+            // 注意返回 config，否则无法执行
+            return config
+          },
+        },
+      })
+
+      // 不推荐直接用 _axiosInstance 发起请求，后面会优化
+      vAxios._axiosInstance
+        .request({
+          url: '/api/data',
+        })
+        .then((res) => {
+          console.log('res: ', res)
+        })
+    }
+
+    const handleTestResponseInterceptor = () => {
+      const vAxios = new VAxios({
+        transform: {
+          responseInterceptors(res) {
+            const { data } = res
+            if (data.code == 200) return data.result
+            throw new Error(data.message)
+          },
+        },
+      })
+
+      // 不推荐直接用 _axiosInstance 发起请求，后面会优化
+      vAxios._axiosInstance
+        .request({
+          url: '/api/data',
+        })
+        .then((res) => {
+          console.log('res: ', res)
+        })
+    }
+
+    const handleTestResponseCatchInterceptor = () => {
+      const vAxios = new VAxios({
+        transform: {
+          responseInterceptorsCatch(instance, error) {
+            console.log('捕获异常: ', error)
+            throw error
+          },
+        },
+      })
+
+      // 不推荐直接用 _axiosInstance 发起请求，后面会优化
+      vAxios._axiosInstance.request({
+        url: '/not-found',
+      })
+    }
+
     return {
       handleRequest,
+      handleTestRequestInterceptor,
+      handleTestResponseInterceptor,
+      handleTestResponseCatchInterceptor,
     }
   },
 }
