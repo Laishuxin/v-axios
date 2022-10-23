be956a4ec5428d7a8055b770e6da2c1a2681843b
Laishuxin <15622971239@163.com>
2022-10-23 15:15:09 +0800

    chore: 1. 提供基础的工具

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,46 @@
   <div>
     <h1>Welcome</h1>
   </div>
+
+  <div>
+    <h1>使用原始的 axios 发起请求</h1>
+    <el-button @click="handleRequest">
+      使用原始的 axios 发起请求
+    </el-button>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
 export default {
   setup() {
-    return {}
+    const handleRequest = () => {
+      axios
+        .request({
+          method: 'get',
+          url: '/api/data',
+        })
+        .then((res) => {
+          const data = res.data
+          if (data.code != 200) {
+            throw new Error(data.message)
+          } else {
+            return data.result
+          }
+        })
+        .then((data) => {
+          ElMessage({
+            type: 'success',
+            message: `请求成功，数据为：${JSON.stringify(data)}`,
+          })
+        })
+    }
+
+    return {
+      handleRequest,
+    }
   },
 }
 </script>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000/api', // 后端服务实际地址
+        changeOrigin: true, //开启代理
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
